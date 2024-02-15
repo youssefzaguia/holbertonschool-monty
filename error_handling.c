662b7b5810ee68ced4470cfe0f1bc29d77197724
youssefzaguia <youssefultras02tn@gmail.com>
2024-02-15 18:07:05 +0100

    error handling

---
+++ error_handling.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+/**
+ * check_arguments - check if there is are 2 cases in av**
+ * @ac: int
+ * Return: true if exists, false otherwise
+ */
+bool check_arguments(int ac)
+{
+    if (ac != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        return (false);
+    }
+    return (true);
+}
